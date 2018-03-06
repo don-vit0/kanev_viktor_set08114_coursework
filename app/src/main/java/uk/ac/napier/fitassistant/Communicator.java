9954e8f63e8a3ede06a2aeddd294b52b2c5e50d7
don-vit0 <35965350+don-vit0@users.noreply.github.com>
2018-03-06 19:34:54 +0000

    implemented BMI calculator

---
+++ app/src/main/java/uk/ac/napier/fitassistant/Communicator.java
@@ -0,0 +1,8 @@
+package uk.ac.napier.fitassistant;
+
+/**
+ * Created by vikto on 3/3/2018.
+ */
+
+public class Communicator {
+}
---
+++ app/src/main/java/uk/ac/napier/fitassistant/WeightConvFragment.java
@@ -0,0 +1,29 @@
+package uk.ac.napier.fitassistant;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WeightConvFragment extends Fragment {
+
+
+    public WeightConvFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weight_conv, container, false);
+    }
+
+}
